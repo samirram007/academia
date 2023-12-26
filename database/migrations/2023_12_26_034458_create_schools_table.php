aae2fe5d91fa332aa9d6918e8189989e13a006a0
Samir Ram <samirram007@gmail.com>
2023-12-26 22:17:41 +0530

    sync

--- database/migrations/2023_12_26_034458_create_schools_table.php
+++ database/migrations/2023_12_26_034458_create_schools_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('schools', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->require();
+            $table->string('address')->nullable();
+            $table->string('contact_no')->unique()->nullable();
+            $table->string('email')->unique()->nullable();
+            $table->date('establishment_date')->nullable();
+            $table->foreignId(column:'school_type_id')
+                    ->constrained(table:'school_types')
+                    ->onDelete(action:'cascade');
+            $table->string('logo_image')->default(value:'/images/default_logo.png');
+
             $table->timestamps();
         });
     }
--- database/migrations/2023_12_26_054012_create_periods_table.php
+++ database/migrations/2023_12_26_054012_create_periods_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('school_types', function (Blueprint $table) {
+        Schema::create('periods', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('school_types');
+        Schema::dropIfExists('periods');
     }
 };
