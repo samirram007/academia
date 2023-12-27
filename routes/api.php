f9379e9fb222d40f571f9499462f81d41c1ba550
Samir Ram <samirram007@gmail.com>
2023-12-27 21:09:55 +0530

    api_update

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-    Route::post('/logout', [AuthController::class, 'logout']);
     Route::apiResource('/users', UserController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/signup', [AuthController::class, 'signup']);
